57ec6de54635e3d4421d51fee9f748fae7b49ae9
Stw <47381003+StwUser@users.noreply.github.com>
2019-05-13 17:20:27 +0300

    vll

---
+++ bin/Debug/scripts/cScripts.js
@@ -0,0 +1,22 @@
+
+home.onclick = function() {
+	//location.href = 'http://www.yandex.ru/';
+	location.href='cSharpPage.html';
+  };
+  
+about.onclick = function() {
+	location.href='cPlusPlusPage.html';
+  };
+  
+work.onclick = function() {
+	location.href='jsPage.html';
+  };
+  
+contact.onclick = function() {
+	location.href='index.html';
+  };  
+
+//C++ Page
+contr.onclick = function(){
+	location.href='images/controls.png';
+}
---
+++ bin/Debug/scripts/menuScript.js
@@ -0,0 +1,11 @@
+
+
+$(document).ready(function(){
+    $("#menuButton").click(function(){
+      $(".otherButton1").toggle(500);
+      $(".otherButton2").toggle(700);
+      $(".otherButton3").toggle(900);
+      $(".otherButton4").toggle(1000);
+    });
+});
+ 
